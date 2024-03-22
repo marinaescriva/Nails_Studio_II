e59bb67d8062d26ccbf29acb1b1f24736f5b7826
Marina <marinaescriva.24@gmail.com>
2024-03-22 18:18:28 +0100

    feat: edit Navigate if url doesn't exist

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate} from "react-router-dom";
 import { Home } from "../Home/Home";
 import { Login } from "../Login/Login";
 import { Register } from "../Register/Register";
@@ -7,6 +7,7 @@ export const Body = () => {
 
     return (
         <Routes>
+            <Route path='*' element = {<Navigate to = {"/"} replace/>}/>
             <Route path='/' element = {<Home />} />
             <Route path='/login' element = {<Login />} />
             <Route path='/register' element = {<Register />} />
