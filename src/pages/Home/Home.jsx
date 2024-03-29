646690ce58cdbbfb8a9ea9da8af20eca4f30eda1
Marina <marinaescriva.24@gmail.com>
2024-03-29 16:28:21 +0100

    feature: redesign Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import { CDropdown } from '../../common/CDropDown/CDropDown'
-import {CButton} from '../../common/CButton/CButton'
+import { CButton } from '../../common/CButton/CButton'
 import { getStudioServices } from '../../services/apiCalls'
 import './Home.css'
 import '../../common/CDropDown/CDropDown.css'
@@ -11,15 +11,6 @@ export const Home = () => {
     const [loadedData, setLoadedData] = useState(false);
 
     const [studioServices, setStudioServices] = useState([])
-
-    // const newAppointment = async () => {
-    //     try {
-    //         const createNew = await CreateAppointment ()
-    //     } catch (error) {
-    //         console.log(error)
-            
-    //     }
-    // }
 
     useEffect(() => {
 
@@ -42,19 +33,23 @@ export const Home = () => {
 
     return (
         <>
-            <div className='homeDesign'>soy home</div>
-            <select className={"a"} onChange={()=>{}} name={"a"}>
-            <option value="" disabled selected> {"b"} </option>
-            {studioServices.map((item, index) => (
-                <option key={index} value={item.id} className={"c"}>{item.name}</option>
-            ))}
-        </select>
-            {/* <CButton
-            className={"ButtonDrop"}
-            title={"view1"}
-            functionEmit={newAppointment}
+            <div className='homeDesign'>
+                <div><h2>Conócenos</h2>
+                    <div>
+                        <h5>Nos encargamos de hacerte una manicura y perfecta con preciosos diseños, siempre cuidando la seguridad de cada clienta. </h5>
+                        <h5> Disponemos de las mejores técnicas y creamos los diseños más modernos del mundo de las uñas.</h5>
 
-            /> */}
+
+                        <h4>Condiciones de reserva </h4>
+                        <h5>
+                            Anticipo de reserva: Se piden 15€ de anticipo a la hora de hacer la reserva. La reserva se  aplicará al precio total del coste de la manicura.
+                        </h5>
+                        <h5>Cancelación de cita : Se puede cancelar la cita con 48 horas de antelación sin ningún coste. Cancelar dentro de las 48 horas previas a la cita, los 15€ de la reserva se cobrarán sin posibilidad de devolución.
+                        </h5>
+                        <h5>Cambio de cita: Es posible cambiar la cita reservada, siempre con 48 horas de antelación. Pasadas las 48 horas, no será posible cambiar el día y fecha de la cita.
+                            Rogamos asistir a tiempo a su cita, porque dejamos el tiempo exacto para cada servicio, por lo tanto si el retraso es mas de 15 minutos se realizara lo que deje el tiempo hasta acabar el servicio por respecto a la siguente clienta.</h5>
+                    </div></div>
+            </div>
         </>
     )
 }
